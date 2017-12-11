f215736b37b770cf90fe73652c1488403fede05a
Mosquito(A) <skykim10908@naver.com>
2017-12-11 16:03:26 +0900

    20171211 수업

---
+++ src/kr/or/dgit/designpattern_study/ch01/association/Person.java
@@ -0,0 +1,11 @@
+package kr.or.dgit.designpattern_study.ch01.association;
+
+public class Person {
+	private Phone[] phones;
+
+	public Person() {
+		phones = new Phone[2];
+	}
+	
+	
+}
---
+++ src/kr/or/dgit/designpattern_study/ch01/association/PersonTwo.java
@@ -0,0 +1,6 @@
+package kr.or.dgit.designpattern_study.ch01.association;
+
+public class PersonTwo {
+	private Phone homePhones;
+	private Phone officePhone;
+}
